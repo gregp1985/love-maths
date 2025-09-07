db31f713b6266cad282ef378b85283ab8f88dc0e
gregp1985 <greg@gwphillips.co.uk>
2025-09-07 16:14:51 +0100

    increment correct and incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,8 +32,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Aw... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -52,9 +54,13 @@ function calculateCorrectAnswer() {
     }
 }
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 function displayAdditionQuestion(operand1, operand2) {
